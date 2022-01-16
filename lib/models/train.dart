2bcc5fc223cb3156e992f056506ae4ff9f5c9235
Fabien CAYRE <fabiencayre81@gmail.com>
2022-01-16 12:01:43 +0100

    Ajout fromSerial

--- lib/models/train.dart
+++ lib/models/train.dart
@@ -1,5 +1,7 @@
 enum SegmentType { AMRAP, FORTIME, EMOM, TABATA, REST }
 
+class ExerciceType {}
+
 class ExerciceData {
   int weightTotal = 10;
   bool weightX2 = false;
@@ -7,6 +9,18 @@ class ExerciceData {
   bool eachSide = false;
 
   ExerciceData(this.weightTotal, this.weightX2, this.repetition, this.eachSide);
+
+  ExerciceData.fromSerial(String serial) {
+    List<String> data = serial.split(",");
+    weightTotal = int.parse(data[0]);
+    weightX2 = data[1] == "true" ? true : false;
+    repetition = int.parse(data[2]);
+    eachSide = data[3] == "true" ? true : false;
+  }
+
+  String serial() {
+    return "$weightTotal,$weightX2,$repetition,$eachSide";
+  }
 }
 
 class Segment {
@@ -18,24 +32,77 @@ class Segment {
 
   Segment(this.id, this.duration, this.type, this.exercices, this.data);
 
+  Segment.fromSerial(Map<String, dynamic> data) : id = 0 {
+    id = data["id"];
+    duration = data["duration"];
+    type = SegmentType.values[data["type"]];
+
+    String ex = data["exercices"];
+    for (var exercice in ex.split(";")) {
+      List<String> vals = exercice.split("!");
+      int exerciceId = int.parse(vals[0]);
+      ExerciceData values = ExerciceData.fromSerial(vals[1]);
+      exercices[exerciceId] = values;
+    }
+  }
+
   Map<String, dynamic> toMap() {
+    String exos = exercices.entries
+        .map((e) => e.key.toString() + "!" + e.value.serial())
+        .join(";");
     return {
       'id': id,
       'duration': duration,
       'data': data.join(','),
-      'type': type.index
+      'type': type.index,
+      'exercices': exos
     };
+  }
+}
+
+class Session {
+  int id;
+  DateTime dateTime;
+  List<Segment> sessions = [];
+
+  Session(this.id, this.dateTime);
+
+  Session.fromSerial(Map<String, dynamic> data)
+      : id = 0,
+        dateTime = DateTime.now() {
+    id = data["id"];
+    dateTime = data["dateTime"];
+    sessions = [];
+  }
+
+  Map<String, dynamic> toMap() {
+    return {"id": id, "dateTime": dateTime.millisecondsSinceEpoch};
   }
 }
 
 class Week {
   int id;
   DateTime dateTime;
-  Map<Segment, bool> sessions = {};
+  List<Session> sessions = [];
 
   Week(this.id, this.dateTime);
 
+  Week.fromSerial(Map<String, dynamic> data)
+      : id = 0,
+        dateTime = DateTime.now() {
+    id = data["id"];
+    dateTime = data["dateTime"];
+  }
+
+  int get uid {
+    return id;
+  }
+
+  DateTime get date {
+    return dateTime;
+  }
+
   Map<String, dynamic> toMap() {
-    return {'id': id, 'datetime': dateTime};
+    return {'id': id, 'dateTime': dateTime.millisecondsSinceEpoch};
   }
 }
