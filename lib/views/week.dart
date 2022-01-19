ee09336d44e1c8dda2ffd8d618c171d13eb291ef
FabienCAYRE <fabiencayre81@gmail.com>
2022-01-19 11:37:42 +0100

    Affichage de la liste de session d'une semaine

--- lib/views/week.dart
+++ lib/views/week.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:musclatax/components/container.dart';
+import 'package:musclatax/components/title.dart';
 import 'package:musclatax/models/access.dart';
 import 'package:musclatax/models/train.dart';
 
 class WeekDisplay extends StatefulWidget {
   final Week week;
 
-  
   const WeekDisplay({Key? key, required this.week}) : super(key: key);
 
   @override
@@ -38,25 +39,62 @@ class _WeekDisplayState extends State<WeekDisplay> {
     List<Session> _sessions = sessions;
 
     return Scaffold(
-        body: Row(
-      children: [
-        Text("Semaine n°${widget.week.id}: "),
-        ListView.separated(
-          padding: const EdgeInsets.all(8),
-          itemCount: _sessions.length,
-          itemBuilder: (BuildContext context, int index) {
-            Session extracted = _sessions[index];
-            return InkWell(
-              child: Session.widget(extracted),
-              onTap: () => {
-                // Launch the week view
-              },
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
+        body: DefaultContainer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              HeaderText(
+                text: widget.week.toString(),
+                fontSize: 28,
+              ),
+              Flexible(
+                child: ListView.separated(
+                  padding: const EdgeInsets.all(8),
+                  itemCount: _sessions.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    Session extracted = _sessions[index];
+                    return InkWell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Session.widget(extracted),
+                          IconButton(
+                            onPressed: () {
+                              DBAccess.removeSession(widget.week, extracted);
+                              _initAsyncState();
+                            },
+                            icon: const Icon(
+                              Icons.delete,
+                            ),
+                            color: Colors.red,
+                            iconSize: 28,
+                          )
+                        ],
+                      ),
+                      onTap: () => {
+                        // Launch the session view
+                      },
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const Divider(),
+                ),
+              ),
+            ],
+          ),
         ),
-      ],
-    ));
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          elevation: 5,
+          onPressed: () async {
+            Session newSession = Session(-1, DateTime.now());
+            newSession.id =
+                await DBAccess.insertNewSession(widget.week, newSession);
+            _initAsyncState();
+          },
+        ));
   }
 }
