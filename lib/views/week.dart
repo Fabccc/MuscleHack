d3458ea1cc953d10a392e2f3f25ffd9495ec0a33
FabienCAYRE <fabiencayre81@gmail.com>
2022-01-25 13:20:29 +0100

    Ajout menu choix d'un jour de la semaine pour une session

--- lib/views/week.dart
+++ lib/views/week.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:musclatax/components/button.dart';
 import 'package:musclatax/components/container.dart';
 import 'package:musclatax/components/title.dart';
+import 'package:musclatax/components/utils.dart' as mu_utils;
 import 'package:musclatax/models/access.dart';
 import 'package:musclatax/models/train.dart';
 
@@ -26,6 +28,7 @@ class _WeekDisplayState extends State<WeekDisplay> {
   void _initAsyncState() async {
     var _sessions = await _fetchData();
     setState(() {
+      _sessions.sort((s1, s2) => s1.dateTime.compareTo(s2.dateTime));
       sessions = _sessions;
     });
   }
@@ -53,21 +56,45 @@ class _WeekDisplayState extends State<WeekDisplay> {
                   itemCount: _sessions.length,
                   itemBuilder: (BuildContext context, int index) {
                     Session extracted = _sessions[index];
+
+                    Color getColor(Set<MaterialState> states) {
+                      return extracted.done
+                          ? Colors.green.shade400
+                          : Colors.grey.shade600;
+                    }
+
                     return InkWell(
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Session.widget(extracted),
-                          IconButton(
-                            onPressed: () {
-                              DBAccess.removeSession(widget.week, extracted);
-                              _initAsyncState();
-                            },
-                            icon: const Icon(
-                              Icons.delete,
-                            ),
-                            color: Colors.red,
-                            iconSize: 28,
+                          Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  DBAccess.removeSession(
+                                      widget.week, extracted);
+                                  _initAsyncState();
+                                },
+                                icon: const Icon(
+                                  Icons.delete,
+                                ),
+                                color: Colors.red,
+                                iconSize: 28,
+                              ),
+                              Checkbox(
+                                onChanged: (val) {
+                                  extracted.done = val!;
+                                  DBAccess.updateSession(
+                                      widget.week, extracted);
+                                  _initAsyncState();
+                                },
+                                checkColor: Colors.white,
+                                fillColor:
+                                    MaterialStateProperty.resolveWith(getColor),
+                                value: extracted.done,
+                              )
+                            ],
                           )
                         ],
                       ),
@@ -90,11 +117,147 @@ class _WeekDisplayState extends State<WeekDisplay> {
           ),
           elevation: 5,
           onPressed: () async {
-            Session newSession = Session(-1, DateTime.now());
-            newSession.id =
-                await DBAccess.insertNewSession(widget.week, newSession);
+            final value = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => _AddSessionInWeek(
+                          week: widget.week,
+                          sessions: _sessions,
+                        )));
             _initAsyncState();
           },
         ));
+  }
+}
+
+class _AddSessionInWeek extends StatelessWidget {
+  final Week week;
+  final List<Session> sessions;
+
+  const _AddSessionInWeek(
+      {Key? key, required this.week, required this.sessions})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime startOfWeek = mu_utils.DateUtils.startOfWeekDate(week.dateTime);
+
+    isPresent(int day) {
+      return sessions
+          .any((element) => element.dateTime.day == startOfWeek.day + day);
+    }
+
+    if (kDebugMode) {
+      print("___________________________");
+      print("Lundi actif ? ${isPresent(0)}");
+      print("Mardi actif ? ${isPresent(1)}");
+      print("Mercredi actif ? ${isPresent(2)}");
+      print("Jeudi actif ? ${isPresent(3)}");
+      print("Vendredi actif ? ${isPresent(4)}");
+      print("Samedi actif ? ${isPresent(5)}");
+      print("Dimanche actif ? ${isPresent(6)}");
+    }
+
+    addSession(int day) {
+      return isPresent(day)
+          ? null
+          : () async {
+              DateTime dateSession = startOfWeek.add(Duration(days: day));
+              Session newSession = Session(-1, dateSession);
+              newSession.id = await DBAccess.insertNewSession(week, newSession);
+              Navigator.pop(context);
+            };
+    }
+
+    return Scaffold(
+      body: DefaultContainer(
+        topbottom: 80,
+        child: Stack(children: [
+          HeaderText(text: "Ajouter une session"),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  DefaultButton(
+                    content: "Lundi",
+                    fontSize: 28,
+                    paddingLeftRight: 20,
+                    paddingTopBottom: 20,
+                    disabled: isPresent(0),
+                    onPressed: addSession(0),
+                  ),
+                  DefaultButton(
+                    content: "Mardi",
+                    fontSize: 28,
+                    paddingLeftRight: 20,
+                    paddingTopBottom: 20,
+                    disabled: isPresent(1),
+                    onPressed: addSession(1),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  DefaultButton(
+                    content: "Mercredi",
+                    fontSize: 28,
+                    paddingLeftRight: 20,
+                    paddingTopBottom: 20,
+                    disabled: isPresent(2),
+                    onPressed: addSession(2),
+                  ),
+                  DefaultButton(
+                    content: "Jeudi",
+                    fontSize: 28,
+                    paddingLeftRight: 20,
+                    paddingTopBottom: 20,
+                    disabled: isPresent(3),
+                    onPressed: addSession(3),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  DefaultButton(
+                    content: "Vendredi",
+                    fontSize: 28,
+                    paddingLeftRight: 20,
+                    paddingTopBottom: 20,
+                    disabled: isPresent(4),
+                    onPressed: addSession(4),
+                  ),
+                  DefaultButton(
+                    content: "Samedi",
+                    fontSize: 28,
+                    paddingLeftRight: 20,
+                    paddingTopBottom: 20,
+                    disabled: isPresent(5),
+                    onPressed: addSession(5),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  DefaultButton(
+                    content: "Dimanche",
+                    fontSize: 28,
+                    paddingLeftRight: 20,
+                    paddingTopBottom: 20,
+                    disabled: isPresent(6),
+                    onPressed: addSession(6),
+                  )
+                ],
+              )
+            ],
+          ),
+        ]),
+      ),
+    );
   }
 }
