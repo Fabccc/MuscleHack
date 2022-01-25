26279489f821c4434f70ce6918baee22bdfc8f18
FabienCAYRE <fabiencayre81@gmail.com>
2022-01-25 13:19:42 +0100

    Ajout de plus de personnalisation de titre

--- lib/components/title.dart
+++ lib/components/title.dart
@@ -2,18 +2,26 @@ import 'package:flutter/widgets.dart';
 
 class HeaderText extends StatelessWidget {
   final String text;
-  double fontSize = 36;
+  double fontSize;
+  double paddingBottom;
 
-  HeaderText({Key? key, required this.text, this.fontSize = 36})
+  HeaderText(
+      {Key? key,
+      required this.text,
+      this.fontSize = 36,
+      this.paddingBottom = 25})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(text,
-        style: TextStyle(
-            fontFamily: "Ubuntu",
-            color: const Color(0xff4a8cfc),
-            fontSize: fontSize));
+    return Container(
+      margin: EdgeInsets.only(bottom: paddingBottom),
+      child: Text(text,
+          style: TextStyle(
+              fontFamily: "Ubuntu",
+              color: const Color(0xff4a8cfc),
+              fontSize: fontSize)),
+    );
   }
 }
 
