305f1bff8bcc50ae8bd3dc6af1033f52b89b86d0
FabienCAYRE <fabiencayre81@gmail.com>
2022-01-25 14:59:07 +0100

    Ajout fonction color

--- lib/components/utils.dart
+++ lib/components/utils.dart
@@ -1,3 +1,8 @@
+import 'dart:ui';
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 
 class DateUtils {
@@ -13,4 +18,48 @@ class DateUtils {
   static DateTime startOfWeekDate(DateTime dateTime) {
     return dateTime.subtract(Duration(days: dateTime.weekday - 1));
   }
+}
+
+class ColorUtils {
+  static Color colorLuminance(Color color, double lum) {
+    return HSLColor.fromColor(color).withLightness(lum).toColor();
+  }
+
+  static Color colorLuminanceTweaked(Color color, double lum) {
+    // validate hex string
+    String hex = color.toHex().replaceAll("#", '');
+    if (hex.length < 6) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+
+    // convert to decimal and change luminosity
+    String rgb = '#';
+    int tempInt;
+    String tempString;
+    for (int i = 0; i < 3; i++) {
+      tempInt = int.parse(hex.substring(i * 2, i * 2 + 2), radix: 16);
+      tempString =
+          min(max(0, tempInt + tempInt * lum), 255).round().toRadixString(16);
+      rgb += ('00' + tempString).substring(tempString.length);
+    }
+
+    print("Input: ${color.toHex()}, lum: ${lum}, output: ${rgb}");
+    return HexColor.fromHex(rgb);
+  }
+}
+
+extension HexColor on Color {
+  /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
 }
